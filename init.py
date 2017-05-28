55983446be48dfee15c4a1db9807446362242c9a
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2017-05-28 15:26:50 -0300

    init

---
+++ init.py
@@ -0,0 +1,29 @@
+from printer import Printer
+
+# Motor(direction, step)
+
+motorX=['CSID0','CSID1']
+motorY=['CSID2','CSID3']
+motorZ=['CSID4','CSID5']
+nozzle=['CSID6','CSID7']
+
+
+image = [
+  [
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+  ]
+]
+
+
+printerOBJ = Printer(motorX, motorY, motorZ, nozzle, image);
+
+printerOBJ.print_level(0);
+
