35c8d830ef2d6f388293c4853837d0e9c3014470
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2017-05-28 16:06:22 -0300

    step

--- pixel_map.py
+++ pixel_map.py
@@ -1,8 +1,8 @@
 class PixelMap:
   def __init__(self, imageMap):
-    self.Z = len(imageMap)
-    self.X = len(imageMap[0])
-    self.Y = len(imageMap[0][0])
+    self.Z = len(imageMap) -1
+    self.X = len(imageMap[0]) -1
+    self.Y = len(imageMap[0][0]) -1
     print "Z: %s", self.Z
     print "X: %s", self.X
     print "Y: %s", self.Y
@@ -15,6 +15,6 @@ class PixelMap:
 
   def get_pixel(self, x, y, z):
     print '%s, %s, %s', x, y, z
-    
+
     return self.tensor[x][y][z] == 1
 
