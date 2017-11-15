f2368049ba13c6634a590058ba592c992f2e12ae
Anderson Rocha (MAX) <andersonrocha@maxfs.com>
2017-11-15 17:02:41 -0200

    addres adjust and image grew

--- printer.py
+++ printer.py
@@ -8,18 +8,17 @@ class Printer:
     self.motor_Z = Motor(motorZ[0], motorZ[1])
     self.nozzle = Nozzle(nozzle[0], nozzle[1])
     self.image = PixelMap(imageMap)
-  
+
   def print_level(self, z):
     for x in range(0, self.image.X):
       self.motor_X.move_to(x)
-      
+
       for y in range(0, self.image.Y):
         self.motor_Y.move_to(y)
-      
-        if self.image.get_pixel(x, y, z) : 
+
+        if self.image.get_pixel(x, y, z) :
           self.nozzle.do_print()
-        
+
   def print_levels(self):
     for z in range(0, self.image.Z):
       self.print_level(z)
-
